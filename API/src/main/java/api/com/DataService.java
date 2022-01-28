17ba3fee226ecaf6a65a0686be852c611baa2c42
Admin <Admin@DESKTOP-D1FQITM>
2022-01-28 12:58:27 +0530

    changes

--- API/src/main/java/api/com/DataService.java
+++ API/src/main/java/api/com/DataService.java
@@ -66,7 +66,7 @@ public class DataService
 	            {
 	            	 CheckDoctors insert=new CheckDoctors();
 		   		     insert.setData(null);
-		   		 	 insert.setMessage("No such data in list");
+		   		 	 insert.setMessage("No such data in lists");
 		   		 	 insert.setStatus("404");
 		   			 return insert;
 	            }
